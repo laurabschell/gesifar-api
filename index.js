f431207c0c694474fc22a2c08127a51232fadeee
Daniel Schell <danielschell@gearlaunch.com>
2023-07-13 19:04:08 -0300

    metodo post para Login

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json({ limit: '10mb' }))
+// const JWT = require('jsonwebtoken')
+// const secretWord = 'Samus#Aran'
+
+const credentials = {
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'login'
+}
+
+app.get('/', (req, res) => {
+	res.send('hola desde tu primera ruta de la Api')
+})
+
+app.post('/api/login', (req, res) => {
+	const { email, password } = req.body
+	const values = [email, password]
+	var connection = mysql.createConnection(credentials)
+	connection.query("SELECT * FROM usuarios WHERE email = ? AND password = ?", values, (err, result) => {
+		if (err) {
+			res.status(500).send(err)
+		} else {
+			if (result.length > 0) {
+				res.status(200).send({
+					"id": result[0].id,
+					"email": result[0].email,
+					"role": result[0].role,
+				})
+			} else {
+				res.status(400).send('Usuario no existe')
+			}
+		}
+	})
+	connection.end()
+})
+
+
+app.listen(4000, () => console.log('hola soy el servidor en 4000:'))
